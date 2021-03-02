d2ae08d2ff537c8608add187211f84921253e17a
Nathalia Garcia <nathalia.gm1@gmail.com>
2021-03-02 11:31:02 -0300

    resistor color duo

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const decodedValue = (colors) => {
+  const selectedColors = colors.slice(0, 2);
+  return Number(selectedColors.map((color) => COLORS.indexOf(color)).join(""));
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
