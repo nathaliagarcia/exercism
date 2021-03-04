2ee796bbc4d5ef6e02081d9ef45093c77a4c5e46
Nathalia Garcia <nathalia.gm1@gmail.com>
2021-03-04 13:25:32 -0300

    triangle

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -4,19 +4,41 @@
 //
 
 export class Triangle {
-  constructor(...sides) {
-    throw new Error('Remove this statement and implement this function');
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
   }
 
   get isEquilateral() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.isInequality === true) return false;
+    return this.a === this.b && this.b === this.c ? true : false;
   }
 
   get isIsosceles() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.isInequality === true) return false;
+    return this.a === this.b || this.a === this.c || this.b === this.c
+      ? true
+      : false;
   }
 
   get isScalene() {
-    throw new Error('Remove this statement and implement this function');
+    if (this.isInequality === true) return false;
+    return this.a !== this.b && this.a !== this.c && this.b !== this.c
+      ? true
+      : false;
+  }
+
+  get isInequality() {
+    if (this.isTriangle === false) return true;
+    return this.a + this.b < this.c ||
+      this.a + this.c < this.b ||
+      this.b + this.c < this.a
+      ? true
+      : false;
+  }
+
+  get isTriangle() {
+    return this.a > 0 && this.b > 0 && this.c > 0 ? true : false;
   }
 }
